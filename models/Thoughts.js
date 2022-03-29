c57a400630633a770713b49d55e717c5fab35d02
sper0054 <teresa.sperl@gmail.com>
2022-03-29 16:13:03 -0500

    models and utils

---
+++ models/Thoughts.js
@@ -0,0 +1,72 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+const dateFormat = require('../utils/dateFormat');
+
+// Reactions Schema
+const ReactionsSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: ()=> new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+// Thoughts Schema
+const ThoughtsSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionsSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// Total count of reactions
+ThoughtsSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+
+const Thoughts = model('Thoughts', ThoughtsSchema);
+
+
+module.exports = Thoughts;
